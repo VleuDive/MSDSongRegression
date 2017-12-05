8c09241cac191aea4b7242909f45a1bfd6012490
VleuDive <hot1999130@gmail.com>
2017-12-05 12:50:01 +0900

    layers and minibatch training added

--- MSDSongRegression.py
+++ MSDSongRegression.py
@@ -3,12 +3,12 @@ import numpy as np
 from tensorflow.contrib.layers.python.layers.initializers import xavier_initializer
 
 xy=np.loadtxt('year_prediction_2.csv',delimiter=',',dtype=np.float32)
-x_data=xy[:,1:-1]
+x_data=xy[:,1:]
 y_data=xy[:,[0]] # Wine dataset은 class 분류가 맨 앞 column에 나와 있으므로 y_data를 맨 앞 column의 값을 잘라 온 것으로 한다.
-train_data=x_data[463715:]
-train_label=y_data[463715:]
-test_data=x_data[51630:]
-test_label=y_data[51630:]
+train_data=x_data[0:463715]
+train_label=y_data[0:463715]
+test_data=x_data[0:51630]
+test_label=y_data[0:51630]
 
 X=tf.placeholder(tf.float32,shape=[None,12])
 Y=tf.placeholder(tf.float32,shape=[None,1])
@@ -45,16 +45,14 @@ recall=tf.reduce_mean(tf.cast(is_correct,dtype=tf.float32))
 #recall 계산식 구현!
 
 training_epochs=50
-batch_size=10000
+batch_size=100
 
+print("Start learning!")
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    for epoch in range(training_epochs):
+    for epoch in range(500):
         avg_cost=0
-        total_batch=int(463715/batch_size)
-        for i in range(total_batch):
-            batch_xs,batch_ys=tf.train.batch([train_data,train_label],batch_size, allow_smaller_final_batch=True)
-            c, _ = sess.run([cost, train], feed_dict={X: batch_xs.eval(), Y: batch_ys.eval(), keep_prob: 0.7})
-            avg_cost += c / total_batch
+        c, _ = sess.run([cost, train], feed_dict={X: train_data, Y: train_label, keep_prob: 0.7})
+        avg_cost += c
         print('Epoch: ', '%04d' % (epoch + 1), 'cost=', '{:.9f}'.format(avg_cost))
     print("Recall: ",recall.eval(session=sess,feed_dict={X:test_data,Y:test_label,keep_prob:1}))
